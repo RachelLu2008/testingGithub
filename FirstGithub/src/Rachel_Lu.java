38a8c04f857cd85cf896d001386e7fcad6a16b32
YouGotBonked <113187422+YouGotBonked@users.noreply.github.com>
2024-02-15 13:46:49 -0500

    Update Rachel_Lu.java

--- FirstGithub/src/Rachel_Lu.java
+++ FirstGithub/src/Rachel_Lu.java
@@ -1,3 +1,7 @@
-public class Rachel_Lu {
-    
+public class Rachel_Lu
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, I hope this messages meets you in well regards,\n Hello Rachel No Middle Name Lu, I must depart now, adios.");
+    }
 }

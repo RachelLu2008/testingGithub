dfa2c9273fdb12acb930b90eaba926af682f2ae7
126Nabil-M <113116225+126Nabil-M@users.noreply.github.com>
2024-02-20 12:22:47 -0500

    Hello

--- FirstGithub/src/Rachel_Lu.java
+++ FirstGithub/src/Rachel_Lu.java
@@ -3,5 +3,7 @@ public class Rachel_Lu
     public static void main(String[] args)
     {
         System.out.println("Hello, I hope this messages meets you in well regards,\nHello Rachel No Middle Name Lu, \nI must depart now, adios.");
+        System.out.println("Howdy Doody Rachel");
+        System.out.println("Howdy Doody other editor, Mathew");
     }
 }
